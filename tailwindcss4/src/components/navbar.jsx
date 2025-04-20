f28625f42deb76503a90e54664df3f68c4d3da52
vasitha1 <sulemfelsi@gmail.com>
2025-04-20 19:24:37 +0000

    responsive navbar complete

--- tailwindcss4/src/components/navbar.jsx
+++ tailwindcss4/src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router'
-import { MdKeyboardArrowDown } from 'react-icons/md';
+import { MdClose, MdKeyboardArrowDown, MdMenu } from 'react-icons/md';
 
 const Navbar = () => {
 
@@ -17,32 +17,29 @@ const Navbar = () => {
 
     const menuItems = {  
         platform: {  
-            title: "Features", // Added quotes for the string  
-            sections: [  
+            title: "Features",
+            items: [  
                 {  
-                    title: "Key Features", // Changed title to be more engaging  
-                    items: [  
-                        {  
-                            name: "Manage Payment Status",  
-                            desc: "✨ Seamlessly get a comprehensive overview of the payment status of clients—across categories and individually."  
-                        },  
-                        {  
-                            name: "Sales",  
-                            desc: "📊 View converted leads and efficiently manage your prospects' status."  
-                        },  
-                        {  
-                            name: "Customer Management",  
-                            desc: "👥 Keep all customer interactions organized in one place for easy access."  
-                        },  
-                        {  
-                            name: "Employee Management",  
-                            desc: "📅 Assign tasks to employees and follow up on their status with ease."  
-                        }  
-                    ]  
+                    name: "Manage Payment Status",  
+                    desc: "✨ Seamlessly get a comprehensive overview of the payment status of clients—across categories and individually."  
+                },  
+                {  
+                    name: "Sales",  
+                    desc: "📊 View converted leads and efficiently manage your prospects' status."  
+                },  
+                {  
+                    name: "Customer Management",  
+                    desc: "👥 Keep all customer interactions organized in one place for easy access."  
+                },  
+                {  
+                    name: "Employee Management",  
+                    desc: "📅 Assign tasks to employees and follow up on their status with ease."  
                 }  
             ]  
         }  
-    } 
+             
+        }  
+         
   return (
     <nav className='bg-black text-white fixed top-0 left-0 right-0 border-b border-white/10'>
         <div className='container mx-auto px-4 py-2 sm:px-6 lg:px-8'>
@@ -73,7 +70,14 @@ const Navbar = () => {
                                         <div className='absolute left-0 mt-2 w-screen max-w-md bg-white rounded-md shadow-lg py-1 text-black'>
                                             <div>
                                                 {
-                                                    key === 'Features' ? (<div> Features Item</div>) : (<div>Other items</div>)
+                                                    menuItems[key].items.map((item, index) => (
+                                                        <Link key={index} to="#">
+                                                            <div className="p-3 hover:bg-gray-100">
+                                                                <p className="font-medium">{item.name}</p>
+                                                                <p className="text-sm text-gray-600">{item.desc}</p>
+                                                            </div>
+                                                        </Link>
+                                                    ))
                                                 }
                                             </div>
                                         </div>
@@ -93,12 +97,68 @@ const Navbar = () => {
                     
 
                 {/* auth btn */}
-                <div>Buttons</div>
+                <div className='hidden md:flex items-center space-x-4'>
+                    <Link to='/login' className='hover:text-yellow-500 hidden xl:block'>Log in</Link>
+                    <Link to='/contactUs' className='hover:text-yellow-500 hidden xl:block'>Contact us</Link>
+                    <Link to='getStarted' className='hover:text-yellow-600 bg-yellow-700 px-4 py-2 rounded-md hover:bg-sky-800 '>Get Started - for free</Link>
+                </div>
 
                 {/* mobile menu bar*/}
-                <div className='md:hidden'>Mobile Menu</div>
+                <div className='md:hidden'>
+                    <button onClick= {toggleMenu} className='inline-flex items-center justify-center p-2 rounded-md hover:text-sky-400'>
+                        {
+                            !isMenuOpen ? (<MdMenu className='block w-6 h-6'/>) : (<MdClose className='block w-6 h-6'/>)
+                        }
+                    </button>
+                </div>
             </div>
         </div>
+
+        {/* mobile menu*/}
+        {
+            isMenuOpen && (
+                <div className='md:hidden'>
+                    <div className='px-2 pt-2 pb-3 space-y-1'>
+                        {
+                            Object.keys(menuItems).map((key) => (
+                                <div className='space-y-2'>
+                                    <button onClick={() => toggleDropdown(key)} className='hover:text-yellow-500 px-3 py-2 rounded-md text-sm font-medium flex items-center'>
+                                        {menuItems[key].title}
+                                        <MdKeyboardArrowDown className={`ml-2 h-5 transition-transformn ${
+                                        activeDropdown === key ? 'transform rotate-180' : ''
+                                        }`}/>
+                                    </button>
+
+                                    {
+                                        activeDropdown === key && (
+                                            <div className='bg-amber-100 text-black'>
+                                                <div className='pl-4'>
+                                                    {
+                                                        menuItems[key].items.map((item, index) => (
+                                                            <Link key={index} to="#" className='py-2'>
+                                                                <div className="p-3 hover:bg-gray-100">
+                                                                    <p className="font-medium">{item.name}</p>
+                                                                    <p className="text-sm text-gray-600">{item.desc}</p>
+                                                                </div>
+                                                            </Link>
+                                                        ))
+                                                    }
+                                                </div>
+                                            </div>
+                                        )
+                                    }
+                                </div>
+                            ))
+                        }
+                        <Link to='/pricing' className='block px-3 py-2 hover:bg-sky-400'>Pricing</Link>
+                        <Link to='testimonials' className='block px-3 py-2 hover:bg-sky-400'>Testimonials</Link>
+                        <Link to='login' className='block px-3 py-2 hover:bg-sky-400'>Log in</Link>
+                        <Link to='contactUs' className='block px-3 py-2 hover:bg-sky-400'>Contact us</Link>
+                        <Link to='getStarted' className='block px-3 py-2 hover:bg-yellow-700 bg-sky-900'>Get Started- for free</Link>
+                    </div>
+                </div>
+            )
+        }
     </nav>
   )
 }
