08d589d942fa986d6310da8e2e3e6a9cc4da4a53
vasitha1 <sulemfelsi@gmail.com>
2025-04-21 00:38:53 +0000

    animated logo completed

--- tailwindcss4/src/App.jsx
+++ tailwindcss4/src/App.jsx
@@ -1,12 +1,16 @@
 import Navbar from "./components/navbar"
 import './App.css'
 import Hero from "./components/Hero"
+import CompanyLogo from "./components/CompanyLogo"
+import FeaturesSection from "./components/FeaturesSection"
 
 function App() {
   return (
     <>
       <Navbar/>
       <Hero/>
+      <CompanyLogo/>
+      <FeaturesSection/>
     </>
   )
 }
